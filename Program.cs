c7d121618b0ac0dcf33f8a58e44973a47582a6c6
natalia <nataliasolcasanovas@gmail.com>
2017-10-13 13:52:59 -0300

    Agregar todook

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Hola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 
+           String nombre = "Natalia"; 
+           int edad = 31;         
+           
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola"+ nombre);
+            Console.WriteLine("¿Cuántos años tenés?");
+            Console.WriteLine(edad);
+
+
+
+            int n = args.Length;
+            switch(n) {
+
+                case 0:
+                     Console.WriteLine("No se pasaron parámetros");
+                break;
+
+                case 1:
+                     Console.WriteLine("Se pasó un parámetro");
+                break;
+
+                default:
+                     Console.WriteLine($"Se pasaron {n} parámetros");
+                break;
+
+            }
+        }
+    }
+}
